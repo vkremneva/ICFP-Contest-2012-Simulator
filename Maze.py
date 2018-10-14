3e17b551cf32bc37d8fbb2551eccacf8841d619b
vkremneva <snowst@bk.ru>
2018-10-14 13:25:36 +0300

    'update' added, does no harm

--- Maze.py
+++ Maze.py
@@ -6,6 +6,13 @@ class Maze:
         self.maze = []
         self.acceptable_chars = ['R', '*', 'L', '.', '#', '\\', 'O', ' ']
 
+    def __str__(self):
+        lines = []
+        for line in self.maze:
+            lines.append(''.join(line))
+
+        return '\n'.join(lines)
+
     def read(self, fname):
         data = np.loadtxt(fname, dtype=str, delimiter='\n', comments=None, usecols=0)
         # print(data)
@@ -25,3 +32,40 @@ class Maze:
                     raise ValueError("Unacceptable char in maze: " + ch)
 
         # print(self.maze)
+
+    def update(self):
+        height = len(self.maze)
+        width = len(self.maze[0])
+        rule1 = '* '
+        rule2 = '* * '
+        rule3 = ' * *'
+        rule4 = '* \\ '
+
+        for i in range(height-1, 0):
+            for j in range(0, width-1):
+                neighbours = str(self.maze[i][j]) + str(self.maze[i][j+1]) + \
+                             str(self.maze[i-1][j]) + str(self.maze[i-1][j+1])
+                if (neighbours[0] == rule1[0]) & (neighbours[1] == rule1[1]):
+                    self.maze[i][j] = ' '
+                    self.maze[i-1][j] = '*'
+                elif (neighbours[2] == rule1[0]) & (neighbours[3] == rule1[1]):
+                    self.maze[i][j+1] = ' '
+                    self.maze[i-1][j+1] = '*'
+                elif neighbours == rule2:
+                    self.maze[i][j] = ' '
+                    self.maze[i-1][j+1] = '*'
+                elif neighbours == rule3:
+                    self.maze[i][j+1] = ' '
+                    self.maze[i-1][j] = '*'
+                elif neighbours == rule4:
+                    self.maze[i][j] = ' '
+                    self.maze[i-1][j+1] = '*'
+
+        # print(self.maze)
+
+
+maze = Maze()
+maze.read('maps\\contest1.map')
+print(maze)
+maze.update()
+print(maze)
