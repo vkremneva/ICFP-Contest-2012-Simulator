5518269ef52376be928ea14cc1b6e376a2bb3f43
vkremneva <snowst@bk.ru>
2018-10-15 14:58:56 +0300

    'state' added, typos fixed

--- Maze.py
+++ Maze.py
@@ -1,4 +1,11 @@
 import numpy as np
+from enum import Enum
+
+
+class State(Enum):
+    WIN = 0
+    LOSE = -1
+    OK = 1
 
 
 class Maze:
@@ -40,32 +47,40 @@ class Maze:
         rule2 = '* * '
         rule3 = ' * *'
         rule4 = '* \\ '
+        fatal1 = '*R'
+        fatal2 = ['* *R', ' *R*', '* \\R']
 
-        for i in range(height-1, 0):
+        for i in range(height-2, 0, -1):
             for j in range(0, width-1):
                 neighbours = str(self.maze[i][j]) + str(self.maze[i][j+1]) + \
-                             str(self.maze[i-1][j]) + str(self.maze[i-1][j+1])
-                if (neighbours[0] == rule1[0]) & (neighbours[1] == rule1[1]):
+                             str(self.maze[i+1][j]) + str(self.maze[i+1][j+1])
+
+                if (neighbours[0] == rule1[0]) & (neighbours[2] == rule1[1]):
                     self.maze[i][j] = ' '
-                    self.maze[i-1][j] = '*'
-                elif (neighbours[2] == rule1[0]) & (neighbours[3] == rule1[1]):
+                    self.maze[i+1][j] = '*'
+                elif (neighbours[1] == rule1[0]) & (neighbours[3] == rule1[1]):
                     self.maze[i][j+1] = ' '
-                    self.maze[i-1][j+1] = '*'
+                    self.maze[i+1][j+1] = '*'
                 elif neighbours == rule2:
                     self.maze[i][j] = ' '
-                    self.maze[i-1][j+1] = '*'
+                    self.maze[i+1][j+1] = '*'
                 elif neighbours == rule3:
                     self.maze[i][j+1] = ' '
-                    self.maze[i-1][j] = '*'
+                    self.maze[i+1][j] = '*'
                 elif neighbours == rule4:
                     self.maze[i][j] = ' '
-                    self.maze[i-1][j+1] = '*'
+                    self.maze[i+1][j+1] = '*'
 
-        # print(self.maze)
+                elif ((neighbours[0] == fatal1[0]) & (neighbours[2] == fatal1[1])) | \
+                        ((neighbours[1] == fatal1[0]) & (neighbours[3] == fatal1[1])) | \
+                        (neighbours in fatal2):
+                    return State.LOSE
+
+        return State.OK
 
 
 maze = Maze()
 maze.read('maps\\contest1.map')
 print(maze)
-maze.update()
+print(maze.update())
 print(maze)
